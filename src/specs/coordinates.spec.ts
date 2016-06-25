aefa4d9c5d4bdf161ae2f52a4c0d395952652c9a
Bogdán Bikics <bogdan.bikics@gmail.com>
2016-06-26 00:42:34 +0200

    Tests for Point added, some added for Direction

--- src/specs/coordinates.spec.ts
+++ src/specs/coordinates.spec.ts
@@ -1,11 +1,71 @@
 /// <reference path="../scripts/coordinates.ts" />
 /// <reference path="../libs/dt-jasmine/jasmine.d.ts" />
 
-describe("Coordinates", function () {
-    it("Point works", function () {
-        
-        // var point = new Point(1, 1);
-        // expect(point.getNeighbor(Direction.NW)).toBe(new Point(0, 2));
-        expect(true).toBe(true);
+describe("Neighbors of point x=1, y=1", () => {
+    const POINT: Point = new Point(1, 1);
+    
+    function comparePoints(expected: Point, actual: Point): void {
+         expect(actual).toEqual(
+            jasmine.objectContaining({
+                x: expected.x,
+                y: expected.y
+            })
+        );
+    }
+    
+    it("NW is x=0, y=1", () => {
+        comparePoints(new Point(0, 1), POINT.getNeighbor(Direction.NW));
+    });
+    it("NE is x=1, y=0", () => {
+        comparePoints(new Point(1, 0), POINT.getNeighbor(Direction.NE));
+    });
+    it("SE is x=2, y=1", () => {
+        comparePoints(new Point(2, 1), POINT.getNeighbor(Direction.SE));
+    });
+    it("SW is x=1, y=2", () => {
+        comparePoints(new Point(1, 2), POINT.getNeighbor(Direction.SW));
+    });
+});
+
+describe("Right rotation of Direction starting from NW", () => {
+    let direction: Direction = Direction.NW;
+
+    it("first it's NE ", () => {
+        direction = Direction.rotateRight(direction);
+        expect(direction).toBe(Direction.NE);  
+    });
+    it("then it's SE ", () => {
+        direction = Direction.rotateRight(direction);
+        expect(direction).toBe(Direction.SE); 
+    });
+    it("then it's SW ", () => {
+        direction = Direction.rotateRight(direction);
+        expect(direction).toBe(Direction.SW); 
+    });
+    it("then it's NW again", () => {
+        direction = Direction.rotateRight(direction);
+        expect(direction).toBe(Direction.NW); 
+    });
+});
+
+describe("Left rotation of Direction starting from NW", () => {
+    let direction: Direction;
+    beforeAll(() => direction = Direction.NW);
+
+    it("first it's SW ", () => {
+        direction = Direction.rotateLeft(direction);
+        expect(direction).toBe(Direction.SW);  
+    });
+    it("then it's SE ", () => {
+        direction = Direction.rotateLeft(direction);
+        expect(direction).toBe(Direction.SE); 
+    });
+    it("then it's NE ", () => {
+        direction = Direction.rotateLeft(direction);
+        expect(direction).toBe(Direction.NE); 
+    });
+    it("then it's NW again", () => {
+        direction = Direction.rotateLeft(direction);
+        expect(direction).toBe(Direction.NW); 
     });
 });
